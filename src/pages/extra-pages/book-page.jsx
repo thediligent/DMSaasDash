f998d90e77a383e3b4ad5cd5d6304be1537d3644
Daniel <thedilligent@gmail.com>
2024-07-07 22:33:41 -0400

    updated book page

--- src/pages/extra-pages/book-page.jsx
+++ src/pages/extra-pages/book-page.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 // material-ui
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Chip from '@mui/material/Chip';
+import Button from '@mui/material/Button';
 import Skeleton from '@mui/material/Skeleton';
 
 // project import
@@ -43,26 +45,26 @@ function BookPage() {
   return (
     <Grid container rowSpacing={4.5} columnSpacing={2.75}>
       <Grid item xs={12} sx={{ mb: -2.25 }}>
-        <Typography variant="h5">Books</Typography>
+        <Typography variant="h5">TLOTR Books </Typography>
+          <Button target="_blank"href="https://openlibrary.org/search.json?q=the+lord+of+the+rings"> https://openlibrary.org/search.json?q=the+lord+of+the+rings </Button>  
       </Grid>
 
       {loading
         ? Array.from(new Array(8)).map((_, index) => <SkeletonCard key={index} />)
         : books.map((book) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={book.key}>
-              <MainCard>
-                <Typography variant="h5">{book.title}</Typography>
-                <Typography variant="body2"> &nbsp;</Typography>
-                <Typography variant="body2">
-                  Author: {book.author_name ? book.author_name.join(', ') : 'Unknown'}
-                </Typography>
-                <Typography variant="body2">
-                  First Publish Year: {book.first_publish_year || 'Unknown'}
-                </Typography>
-                <Typography variant="body2"> &nbsp;</Typography>
-              </MainCard>
-            </Grid>
-          ))}
+          <Grid item xs={12} sm={6} md={4} lg={3} key={book.key}>
+            <MainCard>
+              <Typography variant="h5">{book.title}</Typography>
+              <Typography variant="body2"> &nbsp;</Typography>
+              <Typography variant="body2">
+                {book.author_name ? book.author_name.join(', ') : 'Unknown'}
+              </Typography>
+              <Typography variant="body2"> &nbsp;</Typography>
+              <Chip label={book.first_publish_year || 'Unknown'} variant="outlined" />
+              <Typography variant="body2"> &nbsp;</Typography>
+            </MainCard>
+          </Grid>
+        ))}
     </Grid>
   );
 }
